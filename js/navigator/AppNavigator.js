5841daec60c95c60275a1deecbbe38fa37627840
liukai <651002081@qq.com>
2019-06-19 23:46:25 +0800

    Navigator

---
+++ js/navigator/AppNavigator.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+    createStackNavigator,
+    createBottomTabNavigator,
+    createSwitchNavigator,
+    createMaterialTopTabNavigator
+} from 'react-navigation';
+import WelcomePage from '../page/WelcomePage';
+import HomePage from '../page/HomePage';
+import DetailPage from '../page/DetailPage';
+
+
+const InitNavigator = createStackNavigator({
+    WelcomePage: {
+        screen: WelcomePage,
+        navigationOptions: {
+            header: null // 通过header这是为null禁用顶部标题
+        }
+    }
+});
+
+const MainNavigator = createStackNavigator({
+    HomePage: {
+        screen: HomePage,
+        navigationOptions: {
+            header: null // 通过header这是为null禁用顶部标题
+        }
+    },
+    DetailPage: {
+        screen: DetailPage,
+        navigationOptions: {
+            header: null // 通过header这是为null禁用顶部标题
+        }
+    }
+});
+
+export default createSwitchNavigator({
+    Init: InitNavigator,
+    Main: MainNavigator,
+}, {
+    navigationOptions: {
+        header: null // 通过header这是为null禁用顶部标题
+    },
+})
+
--- js/page/WelcomePage.js
+++ js/page/WelcomePage.js
@@ -9,41 +9,46 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class WelcomePage extends Component<Props> {
+    componentDidMount() {
+        // this.timer = setTimeout(() => { //停留两秒跳转主页
+        //     const {navigation} = this.props;
+        //     navigation.navigate("Main")
+        //
+        // }, 2000)
+    }
+
+    componentWillMount() {  //时间没到的时候关闭组件，导致泄露，所有需要关闭time
+        // this.timer && clearTimeout(this.timer)
+    }
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>WelcomePage</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
