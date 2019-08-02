226d547ff66adfd3a33011ff2f9b648f8eed05b0
liukai <651002081@qq.com>
2019-08-02 13:54:00 +0800

    增加我的模块

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -7,11 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {Button, Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Button, ScrollView, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import NavigationUtil from "../navigator/NavigationUtil";
 import NavigationBar from '../common/NavigationBar';
 import Feather from 'react-native-vector-icons/Feather'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import {MORE_MENU} from "../common/MORE_MENU";
+import GlobalStyles from "../res/GlobalStyles";
+import ViewUtil from "../util/ViewUtil";
 
 const THEME_COLOR = "#678";
 type Props = {};
@@ -93,6 +96,14 @@ export default class MyPage extends Component<Props> {
         </TouchableOpacity>
     }
 
+    onClick(menu) {
+
+    }
+
+    getItem(menu) {
+        return ViewUtil.getMenuItem(() => this.onClick(menu), menu, THEME_COLOR);
+    }
+
     render() {
         let statusBar = {
             backgroundColor: THEME_COLOR,
@@ -108,44 +119,58 @@ export default class MyPage extends Component<Props> {
             />;
         const {navigation} = this.props;
         return (
-            <View style={styles.container}>
+            <View style={GlobalStyles.root_container}>
                 {navigationBar}
-                <Text style={styles.welcome}>MyPage</Text>
-                <Button title={'改变主题颜色'}
-                        onPress={() => {
-                            navigation.setParams({
-                                theme: {
-                                    tintColor: 'blue',
-                                    updateTime: new Date().getTime()
-                                }
-                            })
-                        }}/>
-                <Text onPress={() => {
-                    NavigationUtil.goPage({
-                        navigation: this.props.navigation
-                    }, "DetailPage")
-                }}>跳转到详情</Text>
-                <Button
-                    title={"Fetch 使用"}
-                    onPress={() => {
-                        NavigationUtil.goPage({
-                            navigation: this.props.navigation
-                        }, "FetchDemoPage")
-                    }}/>
-                <Button
-                    title={"AsyncStorage 使用"}
-                    onPress={() => {
-                        NavigationUtil.goPage({
-                            navigation: this.props.navigation
-                        }, "AsyncStorageDemoPage")
-                    }}/>
-                <Button
-                    title={"离线缓存框架"}
-                    onPress={() => {
-                        NavigationUtil.goPage({
-                            navigation: this.props.navigation
-                        }, "DataStoreDemoPage")
-                    }}/>
+                <ScrollView>
+                    <TouchableOpacity
+                        style={styles.item}
+                        onPress={() => this.onClick(MORE_MENU.About)}
+                    >
+                        <View style={styles.about_left}>
+                            <Ionicons
+                                name={MORE_MENU.About.icon}
+                                size={40}
+                                style={{
+                                    marginRight: 10,
+                                    color: THEME_COLOR
+                                }}
+                            />
+                            <Text>GitHub Popular</Text>
+                        </View>
+                        <Ionicons
+                            name={'ios-arrow-forward'}
+                            size={16}
+                            style={{
+                                marginRight: 10,
+                                alignItems: 'center',
+                                color: THEME_COLOR
+                            }}
+                        />
+                    </TouchableOpacity>
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Tutorial)}
+
+                    <Text style={styles.groupTitle}>趋势管理</Text>
+                    {this.getItem(MORE_MENU.Custom_Language)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Sort_Language)}
+
+                    <Text style={styles.groupTitle}>最热管理</Text>
+                    {this.getItem(MORE_MENU.Custom_Key)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Sort_Key)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Remove_Key)}
+
+                    <Text style={styles.groupTitle}>设置</Text>
+                    {this.getItem(MORE_MENU.Custom_Theme)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.About_Author)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Feedback)}
+
+                </ScrollView>
+
             </View>
         )
     }
@@ -171,4 +196,24 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    about_left: {
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+
+    },
+    groupTitle: {
+        marginLeft: 10,
+        marginTop: 10,
+        marginBottom: 5,
+        fontSize: 12,
+        color: 'gray'
+    }
 });
--- js/util/ViewUtil.js
+++ js/util/ViewUtil.js
@@ -1,8 +1,55 @@
 import React from 'react'
-import {TouchableOpacity} from 'react-native'
+import {TouchableOpacity, View, StyleSheet, Text} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default class ViewUtil {
+
+    /**
+     * 获取设置页面的item
+     * @param callBack 单击item回调
+     * @param text 显示的文本
+     * @param color 图标颜色
+     * @param Icons react-native-vector-icons组件
+     * @param icon 左侧图标
+     * @param expandableIco  右侧图标
+     */
+    static getSetingItem(callBack, text, color, Icons, icon, expandableIco) {
+        return (
+            <TouchableOpacity
+                onPress={callBack}
+                style={styles.setting_item_container}
+            >
+                <View style={{alignItems: 'center', flexDirection: 'row'}}>
+                    {Ionicons && icon ?
+                        <Icons
+                            name={icon}
+                            size={16}
+                            style={{color: color, marginRight: 10}}
+                        /> :
+                        <View style={{opacity: 1, width: 16, height: 16, marginRight: 10}}/>
+                    }
+                    <Text>{text}</Text>
+                </View>
+                <Ionicons
+                    name={expandableIco ? expandableIco : 'ios-arrow-forward'}
+                    size={16}
+                    style={{
+                        marginRight: 10,
+                        alignSelf: 'center',
+                        // alignItems: 'center',
+                        color: color || 'black',
+                    }}
+                />
+
+            </TouchableOpacity>
+        )
+    }
+
+
+    static getMenuItem(callBack, menu, color, expandableIco) {
+        return ViewUtil.getSetingItem(callBack, menu.name, color, menu.Icons, menu.icon, expandableIco)
+    }
+
     /**
      * 获取左侧按钮
      * @param callBack
@@ -32,7 +79,17 @@ export default class ViewUtil {
                 style={{opacity: 0.9, marginRight: 10, color: 'white'}}
             />
         </TouchableOpacity>
-
     }
-
 }
+
+const styles = StyleSheet.create({
+    setting_item_container: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 60,
+        alignItems: 'center',
+        // justifyContent: 'space-between',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+});
